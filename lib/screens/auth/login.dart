7f1b55971fda5db7f60516fdda090ab3ad71da6c
Neemias Manso <neemias.manso@jcgestaoderiscos.com.br>
2026-03-18 08:49:34 -0300

    ci: add testing pipeline

--- lib/screens/auth/login.dart
+++ lib/screens/auth/login.dart
@@ -1,4 +1,4 @@
-﻿import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:provider/provider.dart';
 import 'package:url_launcher/url_launcher.dart';
@@ -61,7 +61,9 @@ class _LoginScreenState extends State<LoginScreen>
           listen: false,
         ).login(_userController.text, _passwordController.text);
       } catch (e) {
-        if (!mounted) { return; }
+        if (!mounted) {
+          return;
+        }
         ScaffoldMessenger.of(context).showSnackBar(
           SnackBar(
             content: Text(e.toString().replaceAll('Exception: ', '')),
